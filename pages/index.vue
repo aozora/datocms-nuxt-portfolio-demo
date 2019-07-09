b8508faeee9b57533be90783abbfe67e94a499cd
marcellop <marcello.palmitessa@gmail.com>
2019-07-09 14:24:13 +0200

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
            :cols="3"
            :gutter="30"
   >
-    <div v-for="(item, index) in items" :key="index">Item: {{index + 1}}</div>
+    <div v-for="(item, index) in items" :key="index">Item: {{ index + 1 }}</div>
   </masonry>
 </template>
 
@@ -16,7 +16,7 @@ export default {
 
   apollo: {
     allDatoCmsWork: gql`query IndexQuery {
-  work(sort: { fields: [position], order: ASC }) {
+  work(orderBy: position_ASC) {
     edges {
       node {
         id
@@ -24,9 +24,12 @@ export default {
         slug
         excerpt
         coverImage {
-          fluid(maxWidth: 450, imgixParams: { fm: "jpg", auto: "compress" }) {
-            ...GatsbyDatoCmsSizes
-          }
+          width
+          height
+          src
+          #fluid(maxWidth: 450, imgixParams: { fm: "jpg", auto: "compress" }) {
+          #  ...GatsbyDatoCmsSizes
+          #}
         }
       }
     }
@@ -35,38 +38,5 @@ export default {
    `
   }
 
-}
+};
 </script>
-
-<style>
-  .container {
-    margin: 0 auto;
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-  }
-
-  .title {
-    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-    display: block;
-    font-weight: 300;
-    font-size: 100px;
-    color: #35495e;
-    letter-spacing: 1px;
-  }
-
-  .subtitle {
-    font-weight: 300;
-    font-size: 42px;
-    color: #526488;
-    word-spacing: 5px;
-    padding-bottom: 15px;
-  }
-
-  .links {
-    padding-top: 15px;
-  }
-</style>
